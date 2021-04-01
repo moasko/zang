074c8c3353aaf06dbbe5d548f1298be3fbca9a9d
moasko.dev <moasko.dev@gmail.com>
2021-04-01 13:46:45 +0000

    create file contacte components

---
+++ screens/contacteListe.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import {View,Text} from 'react-native'
+
+
+function ContactesListe(){
+    return(
+  <View>
+      <Text>moasko</Text>
+  </View>
+    )
+}
+
+export default ContactesListe
