fc745482da2c4905526cbca551fd242eed5e37f6
moasko.dev <moasko.dev@gmail.com>
2021-05-22 18:22:27 +0000

    v

---
+++ .history/components/SearchBar/SearchBar_20210522134948.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    FlatList,
+    TouchableOpacity,
+    TextInput, Image
+} from 'react-native';
+
+function SearchBar({ value, updateSearch, style }) {
+    const [query, setQuery] = useState();
+    const [error, setError] = useState();
+
+    return (
+        <View style={[styles.container, style]}>
+            <View style={styles.searchContainer}>
+
+                <View style={styles.vwSearch}>
+                    <Image
+                        style={styles.icSearch}
+                        source={require('./ic_search.png')} />
+                </View>
+
+                <TextInput
+                    value={query}
+                    placeholder="Rechercher un produit ..."
+                    style={styles.textInput}
+                    onChangeText={(text) => {
+                        var letters = /^$|^[a-zA-Z._\b ]+$/;
+                        if (text.length > 12)
+                            setError("Requête trop longue.")
+                        else if (text.match(letters)) {
+                            setQuery(text)
+                            updateSearch(text)
+                            if (error)
+                                setError(false)
+                        }
+                        else setError("Veuillez n'entrer que des alphabets")
+                    }}
+                />
+                {
+                    query ?
+                        <TouchableOpacity
+                            onPress={() => setQuery('')}
+                            style={styles.vwClear}>
+                            <Image
+                                style={styles.icClear}
+                                source={require('./ic_clear.png')} />
+                        </TouchableOpacity>
+                        : <View style={styles.vwClear} />
+                }
+
+            </View>
+            {
+                error &&
+                <Text style={styles.txtError}>
+                    {error}
+                </Text>
+            }
+        </View >
+    )
+}
+const styles = StyleSheet.create({
+    txtError: {
+        marginTop: '2%',
+        width: '89%',
+        color: 'white',
+
+    },
+    vwClear: {
+        flex: 0.2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    textInput: {
+        // backgroundColor: 'green',
+        flex: 1,
+    },
+
+    vwSearch: {
+        flex: 0.2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // width: 40,
+        // backgroundColor: 'red'
+    },
+    icSearch: {
+        height: 18, width: 18
+    },
+    searchContainer:
+    {
+        backgroundColor: 'white',
+        width: '90%',
+        height: 40,
+        flexDirection: 'row'
+
+    },
+    container: {
+        marginTop:10,
+        height: 50,
+        alignItems: 'center',
+        // height: '100%', width: '100%' 
+    },
+});
+
+export default SearchBar
---
+++ .history/screens/Panier_20210522172248.js
@@ -0,0 +1,63 @@
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform } from 'react-native';
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+export default function Panier() {
+  const [expoPushToken, setExpoPushToken] = useState('ExponentPushToken[U6jzUPFaLIeVk8xyq1oXq4]');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}>
+      <Text>Your expo push token: {expoPushToken}</Text>
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+      />
+    </View>
+  );
+}
+
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "ZANGOCHAP PROMO",
+      body: '10000% de reduction sur zangochap',
+      data: { data: 'voir' },
+    },
+    trigger: { seconds: 2 },
+  });
+}
